543bf3453680c8c58b9c76e89de572fb0ddf7a7c
Russann Johnson <reachrussann@gmail.com>
2015-10-01 07:45:54 -0600

    Submitting with you teacher thanks...

--- problems/array-sum.js
+++ problems/array-sum.js
@@ -1,7 +1,19 @@
 'use strict';
 
 function arraySum(arr) {
+  var total = 0;  // Variable to hold your total
 
+  for(var i = 0; i < arr.length; i++) {
+      var element = arr[i];
+      if (typeof element === 'object') {
+        total = total + arraySum(element);
+      } else {
+        total = total + element;
+      }
+      // Iterate first array,then grab second element, add values up
+      //recursion
+  }
+
+return total;
 }
-
 module.exports = arraySum;
--- problems/longest-string.js
+++ problems/longest-string.js
@@ -1,7 +1,17 @@
 'use strict';
+/**
+*Array.prototype.length is from:
+*https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/prototype
+*
+*
+*/
 
-function longestString(arr) {
-
-}
-
-module.exports = longestString;
+module.exports =  function(arr) {
+  return arr.sort(function(a,b) {
+    if (typeof a !== 'string'){ return 1; }
+    if (typeof b !== 'string') { return -1; }
+    if (a.length < b.length) { return 1; }
+    if (b.length > a.length){ return -1; }
+    return 0;
+  })[0];
+};
